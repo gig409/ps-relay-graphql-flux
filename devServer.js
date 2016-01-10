3c9a97a40d369d3074669a39544a0f2d01d28401
Garfield Gordon <garfield.gordon@btinternet.com>
2016-01-10 16:50:13 +0000

    Initial commit - react hot loader template + dir structure

--- devServer.js
+++ devServer.js
@@ -1,27 +1,21 @@
-var path = require('path');
-var express = require('express');
-var webpack = require('webpack');
-var config = require('./webpack.config.dev');
+import express from 'express';
+import webpack from 'webpack';
+import config from './webpack.config.dev';
 
-var app = express();
-var compiler = webpack(config);
+let app = express();
+let compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
+app.use(express.static('public'));
+app.get('/', (req, res) => res.send('I\'m Alive!!'));
 
-app.use(require('webpack-hot-middleware')(compiler));
+// Only load this middleware in dev mode (!important)
+if (app.get('env') === 'development') {
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+  app.use(require('webpack-hot-middleware')(compiler));
+}
 
-app.listen(3000, 'localhost', function(err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
-  console.log('Listening at http://localhost:3000');
-});
+app.listen(3000, () => console.log('listening on port 3000'));
